6863299e5b438e4251a2265738a1ccebccb4e910
VoidOutsider <andrew.guest93@gmail.com>
2019-11-12 19:32:52 -0600

    Added SQLAlchemy

--- api.py
+++ api.py
@@ -1,17 +1,29 @@
+from os import getenv
+
 from flask import Flask, jsonify
 from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
 
+
+#########################
+# Configuration / setup #
+#########################
+mysql_username = getenv('MYSQL_USERNAME')
+mysql_password = getenv('MYSQL_PASSWORD')
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://{}:{}@localhost/Japan'.format(mysql_username, mysql_password)
+
 api = Api(app)
+db = SQLAlchemy(app)
+
+
+
 
 
 class TestPing(Resource):
     def get(self):
-        return {
-            'status': 'success',
-            'message': 'pong!'
-        }
+        return jsonify(username=mysql_username, password=mysql_password, URI=app.config['SQLALCHEMY_DATABASE_URI'])
 
 
 api.add_resource(TestPing, '/test/ping')
---
+++ models.py
@@ -0,0 +1,14 @@
+from api import db
+
+
+class Candy(db.Model):
+    candy_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), unique=True)
+    taste = db.Column(db.String(50))
+    region = db.Column(db.String(50))
+    url = db.Column(db.String(200))
+    date_added = db.Column(db.DATETIME)
+    image_path = db.Column(db.String(50))
+
+    def __repr__(self):
+        return '<Candy {}>'.format(self.name)
