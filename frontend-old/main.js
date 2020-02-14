526c093d24fb88da27e5dae2f5185ddaf400f1a7
Andrew Guest <andrew.guest93@gmail.com>
2020-02-13 21:53:19 -0600

    Working prototype of new Vue frontend

---
+++ frontend-old/main.js
@@ -0,0 +1,20 @@
+var app = new Vue({
+    el: '#apiData',
+    data: {
+        foodResults: [],
+        drinkResults: []
+    },
+    filters: {
+        capitalize: function (value) {
+            if (!value) return ''
+            value = value.toString()
+            return value.charAt(0).toUpperCase() + value.slice(1)
+        }
+    },
+    beforeMount() {
+        axios.get("http://api.myjapandb.com/api/japan/food/all")
+        .then(response => {this.foodResults = response.data}),
+        axios.get("http://api.myjapandb.com/api/japan/drinks/all")
+        .then(response => {this.drinkResults = response.data})
+    }
+});
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,93 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container" id="top">
+    <CategorySwitchButtons
+      :foodButtonDisabled="food"
+      :drinkButtonDisabled="drink"
+      @buttonClicked="getAPI"
+    />
+    <div class="columns is-multiline" id="cards">
+      <div class="column is-3" v-for="entry in apiData" :key="entry.id">
+        <div class="card">
+          <div class="card-content">
+            <p class="subtitle">{{ entry['name'] }}</p>
+          </div>
+          <footer class="card-footer">
+            <p class="card-footer-item">
+              <span>
+                <a :href="entry['url']">Purchase here</a>
+              </span>
+            </p>
+          </footer>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CategorySwitchButtons from "./components/CategorySwitchButtons.vue";
+import axios from "axios";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    CategorySwitchButtons
+  },
+  data() {
+    return {
+      apiData: null,
+      food: false,
+      drink: false,
+      lastButtonClicked: null,
+      msg: "Nothing clicked yet"
+    };
+  },
+  methods: {
+    getAPI(buttonName) {
+      if (buttonName === "food") {
+        this.lastButtonClicked = "food";
+        axios
+          .get("http://api.myjapandb.com/api/japan/food/all")
+          .then(response => {
+            this.apiData = response.data;
+            this.msg = response.data;
+          })
+          .catch(e => {
+            console.log(e);
+          });
+      } else if (buttonName === "drink") {
+        this.lastButtonClicked = "drink";
+        axios
+          .get("http://api.myjapandb.com/api/japan/drinks/all")
+          .then(response => {
+            this.apiData = response.data;
+            this.msg = response.data;
+          })
+          .catch(e => {
+            console.log(e);
+          });
+      }
+    },
+    init() {
+      if (localStorage.lastButtonClicked) {
+        this.lastButtonClicked = localStorage.lastButtonClicked;
+        this.getAPI(this.lastButtonClicked);
+      }
+    }
+  },
+  mounted() {
+    this.init();
+  },
+  watch: {
+    lastButtonClicked(newValue) {
+      localStorage.lastButtonClicked = newValue;
+    }
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<style scoped>
+#top {
   margin-top: 60px;
 }
 </style>
