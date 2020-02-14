38b8a7b4ddaee8e3ee3bb047899cda8f82ca54f0
Andrew Guest <andrew.guest93@gmail.com>
2020-02-13 18:55:05 -0600

    Requiring API key for POST requests. Starting new Vue.js frontend from scratch.

--- backend/api.py
+++ backend/api.py
@@ -1,4 +1,5 @@
 import os
+import logging
 
 from flask import Flask
 from flask_restful import Api
@@ -11,15 +12,18 @@ app = Flask(__name__)
 api = Api(app)
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+logging.basicConfig(filename='/var/log/food-myjapandb.log',
+                    level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s'
+                    )
 
 mariadb_uri = 'mysql://{username}:{password}@localhost/Japan'.format(
-               username=os.getenv('MYSQL_USERNAME'),
-               password=os.getenv('MYSQL_PASSWORD'))
+    username=os.getenv('MYSQL_USERNAME'),
+    password=os.getenv('MYSQL_PASSWORD'))
 
 app.config['SQLALCHEMY_DATABASE_URI'] = mariadb_uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_POOL_RECYCLE'] = 500
-
 
 ######################################################
 # Cross Origin Resource Sharing (CORS) configuration #
--- backend/resources.py
+++ backend/resources.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import os
 
 from flask_restful import Resource, reqparse
 from flask import jsonify, json
@@ -32,28 +33,45 @@ class AllJapanFood(Resource):
 class SingleJapanFood(Resource):
     def post(self):
 
-        parser.add_argument('name', type=str, required=True)
-        parser.add_argument('taste', type=str, required=True)
-        parser.add_argument('region', type=str, required=True)
-        parser.add_argument('url', type=str, required=True)
-        parser.add_argument('image_path', type=str)
+        parser.add_argument("key", type=str, required=True)
+        parser.add_argument("name", type=str, required=True)
+        parser.add_argument("taste", type=str, required=True)
+        parser.add_argument("region", type=str, required=True)
+        parser.add_argument("url", type=str, required=True)
+        parser.add_argument("image_path", type=str)
         current_time = datetime.now()
 
         args = parser.parse_args()
-        args['date_added'] = current_time.strftime("%Y-%m-%d %H:%M:%S")
 
-        new_food = Food(name=args['name'], taste=args['taste'],
-                        region=args['region'], url=args['url'],
-                        image_path=args['image_path'],
-                        date_added=args['date_added'])
+        if args["key"] != os.getenv("API_KEY") or args["key"] is None:
+            data = {"message": "Unauthorized"}
+            return app.response_class(
+                response=json.dumps(data), status=401, mimetype="application/json",
+            )
+        else:
+            args["date_added"] = current_time.strftime("%Y-%m-%d %H:%M:%S")
 
-        db.session.add(new_food)
-        db.session.commit()
+            new_food = Food(
+                name=args["name"],
+                taste=args["taste"],
+                region=args["region"],
+                url=args["url"],
+                image_path=args["image_path"],
+                date_added=args["date_added"],
+            )
 
-        return jsonify(name=args['name'], taste=args['taste'],
-                       region=args['region'], url=args['url'],
-                       image_path=args['image_path'],
-                       date_added=args['date_added'])
+            db.session.add(new_food)
+            db.session.commit()
+            db.session.close()
+
+            return jsonify(
+                name=args["name"],
+                taste=args["taste"],
+                region=args["region"],
+                url=args["url"],
+                image_path=args["image_path"],
+                date_added=args["date_added"],
+            )
 
 
 #######################################
@@ -70,8 +88,8 @@ class AllJapanDrinks(Resource):
 # 'Catch all' function for invalid API endpoints #
 ##################################################
 class NotFound(Resource):
-    def get(self, invalidPath=''):
-        data = {'path': invalidPath, 'message': 'This URL is not a valid API endpoint'}
-        return app.response_class(response=json.dumps(data),
-                                  status=404,
-                                  mimetype='application/json')
+    def get(self, invalidPath=""):
+        data = {"path": invalidPath, "message": "This URL is not a valid API endpoint"}
+        return app.response_class(
+            response=json.dumps(data), status=404, mimetype="application/json"
+        )
