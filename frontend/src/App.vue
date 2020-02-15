27afe0ee72132f0df22e5d34d4c50f7c369c7766
Andrew Guest <andrew.guest93@gmail.com>
2020-02-14 22:55:29 -0600

    Made Bulma cards uniform size and added shadows around each card

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,7 +7,7 @@
     />
     <div class="columns is-multiline" id="cards">
       <div class="column is-3" v-for="entry in apiData" :key="entry.id">
-        <div class="card">
+        <div class="card bm--card-equal-height">
           <div class="card-content">
             <p class="subtitle">{{ entry['name'] }}</p>
           </div>
@@ -47,7 +47,7 @@ export default {
       if (buttonName === "food") {
         this.lastButtonClicked = "food";
         axios
-          .get("http://api.myjapandb.com/api/japan/food/all")
+          .get("https://api.myjapandb.com/api/japan/food/all")
           .then(response => {
             this.apiData = response.data;
             this.msg = response.data;
@@ -58,7 +58,7 @@ export default {
       } else if (buttonName === "drink") {
         this.lastButtonClicked = "drink";
         axios
-          .get("http://api.myjapandb.com/api/japan/drinks/all")
+          .get("https://api.myjapandb.com/api/japan/drinks/all")
           .then(response => {
             this.apiData = response.data;
             this.msg = response.data;
@@ -89,5 +89,16 @@ export default {
 <style scoped>
 #top {
   margin-top: 60px;
+}
+.bm--card-equal-height {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  -moz-box-shadow: 3px 3px 5px 6px #ccc;
+  -webkit-box-shadow: 3px 3px 5px 6px #ccc;
+  box-shadow: 3px 3px 5px 6px #ccc;
+}
+.bm--card-equal-height .card-footer {
+  margin-top: auto;
 }
 </style>
